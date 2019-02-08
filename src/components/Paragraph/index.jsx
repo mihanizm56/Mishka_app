ab09d72fd330f9a044666770823f326757e3f929
Michael_Kozhevnikov <mkozhevnikov@technoserv.com>
2019-02-08 11:08:28 +0300

    fixed

--- src/components/Paragraph/index.jsx
+++ src/components/Paragraph/index.jsx
@@ -1,12 +1,12 @@
 // @flow
 import React, { Node } from "react";
-//import { pure } from "recompose";
+import { pure } from "recompose";
 
 type ParagraphProps = {
 	text: string,
 	className: string,
 };
 
-export const Paragraph = ({ text, className }: ParagraphProps): Node => <p className={className}>{text}</p>;
+const ParagraphComponent = ({ text, className }: ParagraphProps): Node => <p className={className}>{text}</p>;
 
-//const Paragraph = pure(ParagraphComponent);
+export const Paragraph = pure(ParagraphComponent);
--- src/components/SVGComponent/index.jsx
+++ src/components/SVGComponent/index.jsx
@@ -1,10 +1,13 @@
 // @flow
 import React from "react";
-import SVG from "react-inlinesvg";
 import "./SVGComponent.css";
 
 type SVGType = {
 	path: string,
 };
 
-export const SVGComponent = ({ path }: SVGType) => <SVG src={`${path}`} className="icon" alt="icon" />;
+export const SVGComponent = ({ path }: SVGType) => (
+	<object type="image/svg+xml" data={`${path}`} className="icon" alt="icon">
+		>Your browser does not support SVGs
+	</object>
+);
