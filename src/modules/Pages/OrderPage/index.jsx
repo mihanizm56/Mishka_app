c54d4c600712bcb40b6d57bc7855dd471004cb78
Michael_Kozhevnikov <mkozhevnikov@technoserv.com>
2019-03-09 22:45:51 +0300

    fixed

--- src/modules/Pages/OrderPage/index.jsx
+++ src/modules/Pages/OrderPage/index.jsx
@@ -1,17 +1,43 @@
 import React, { Component } from "react";
-import { FormLabelParagraph } from "../../../components";
+import { FormLabelParagraph, TextForVideo } from "../../../components";
 import { CheckBox, RadioBox, FormTextInputField } from "../../Cards";
 import "./OrderPage.css";
 
 export class OrderPage extends Component {
 	render() {
 		return (
-			<>
-				<CheckBox />
-				<RadioBox />
-				<FormLabelParagraph text="фио" />
-				<FormTextInputField />
-			</>
+			<div className='order-page-wrapper'>
+				<div className='order-page-container'>
+					<div className='order-page__text'>
+							<TextForVideo text='Мы будем рады воплотить в жизнь ваши пожелания!'/>
+							<TextForVideo text='Заполните простую форму заказа и мы свяжемся с вами, чтобы уточнить детали.'/>
+					</div>
+					<div className='order-page-radio-container'>
+
+					</div>
+					<div className='order-page-check-container'>
+
+					</div>
+					<div className='order-page-name-container'>
+
+					</div>
+					<div className='order-page-contact-container'>
+
+					</div>
+					<div className='order-page-textarea-container'>
+
+					</div>
+					<div className='order-page-send-container'>
+
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
+  
+
+				/* <CheckBox />
+				<RadioBox />
+				<FormLabelParagraph text="фио" />
+				<FormTextInputField /> */
