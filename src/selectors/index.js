0a723bf35e14e8aefa658159d73c250493003380
MKozhevnikov <mkozhevnikov@technoserv.com>
2019-03-31 02:17:44 +0300

    fixed

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,1 +1,6 @@
-export * from "./reviewSelector";
+export * from "./appLoading";
+export * from "./itemsFilters";
+export * from "./loginReducer";
+export * from "./reviews";
+export * from "./shopItems";
+export * from "./userItems";
