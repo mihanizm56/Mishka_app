98d54b4eaeaa5c897a4dba47983b3263bf8b49a4
Michael_Kozhevnikov <mkozhevnikov@technoserv.com>
2019-04-01 22:52:34 +0300

    fixed

--- src/components/atoms/buttons/Button/index.jsx
+++ src/components/atoms/buttons/Button/index.jsx
@@ -11,9 +11,9 @@ type ButtonPropsType = {
 
 export const Button = ({ handleClick, text, classname }: ButtonPropsType) => {
 	return (
-		<div className={classname} onClick={handleClick}>
+		<button className={classname} onClick={handleClick}>
 			<OpenSansText classname='text-button' bold text={text} />
-		</div>
+		</button>
 	);
 };
 
--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,2 +1,3 @@
 export * from "./globalPackage";
 export * from "./pages";
+export * from "./BigForm";
