50d3a87f957def9e4b263d213b753744ac26c755
MKozhevnikov <mkozhevnikov@technoserv.com>
2019-04-09 01:32:40 +0300

    deleted_flow

--- src/actions/contacts/index.js
+++ src/actions/contacts/index.js
@@ -1,16 +1,16 @@
 //
 import { loadingAppDoneAction, loadingAppAction } from "../loading";
-import { getContacts } from '../../constants'
-import { myFetch } from '../../utils/requests'
+import { GET_CONTACTS } from "../../constants";
+import { myFetch } from "../../utils/requests";
 
-export const getContacts = (data) => ({
-    type: getContacts,
-    payload: { ...data }
-})
+export const getContacts = data => ({
+	type: GET_CONTACTS,
+	payload: { ...data },
+});
 
 export const fetchContacts = () => dispatch => {
-    dispatch(loadingAppAction())
-    myFetch('GET', 'localhost:3000/contacts')
-        .then(data => dispatch(getContacts(data)))
-        .then(dispatch(loadingAppDoneAction())
+	dispatch(loadingAppAction());
+	myFetch("GET", "localhost:3000/contacts")
+		.then(data => dispatch(getContacts(data)))
+		.then(dispatch(loadingAppDoneAction()));
 };
